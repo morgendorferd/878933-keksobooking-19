0b139992c67632df9976fa70db42bef33dec74af
dariah <d-hohlova94@mail.ru>
2020-02-01 20:47:34 +0300

    Генерацию объекта и генерацию массива объектов вынесла в отдельные функции

--- js/main.js
+++ js/main.js
@@ -1,154 +1,126 @@
 'use strict';
 
-var adverts = []; //  массив объектов предложений
-var offerNumber; // пордяковый номер предложения
-var x; // координата Х
-var y; // координата У
+var MAX_AMOUNT = 8;
 
-//  генерирует случайную цену
-var generatePrice = function () {
-  var roomPrice = Math.floor(Math.random() * 10000);
-  if (roomPrice === 0) {
-    roomPrice = 10000;
+var MOCK_DATA = {
+  author: {
+    avatar: ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png']
+  },
+
+  offer: {
+    title: ['Предложение 1', 'Предложение 2', 'Предложение 3', 'Предложение 4', 'Предложение 5', 'Предложение 6', 'Предложение 7', 'Предложение 8'],
+    address: '600, 350',
+    price: 10000,
+    type: ['palace', 'flat', 'house', 'bungalo'],
+    rooms: 3,
+    guests: 6,
+    checkin: ['12.00', '13.00', '14.00'],
+    checkout: ['12.00', '13.00', '14.00'],
+    features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+    description: ['Описание 1', 'Описание 2', 'Описание 3', 'Описание 4', 'Описание 5', 'Описание 6', 'Описание 7', 'Описание 8'],
+    photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+  },
+
+  location: {
+    x: 600,
+    y: 350
   }
-  return roomPrice;
 };
 
-//  генерирует тип размещения
-var generateType = function () {
-  var types = ['palace', 'flat', 'house', 'bungalo'];
-  var key = Math.floor(Math.random() * types.length);
-  return types[key];
+// получить случайное число, включающее min и max
+var getRandomIntInclusive = function (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// получить массив случайной длины
+var getRandomArrayLength = function (array) {
+  return array.slice(0, getRandomIntInclusive(1, array.length - 1))
+}
+
+// получить случайный элемент массива
+var getRandomElement = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
-//  генерирует количество комнат
-var generateRoom = function () {
-  var room = Math.floor(Math.random() * 5);
-  if (room === 0) {
-    room = 1;
-  }
-  return room;
-};
+// получить случайный уникальный элемент массива
+var getRandomUniqueElement = function (array) {
+  return array.splice(getRandomIntInclusive(0, array.length - 1), 1)
+}
 
-//  генерирует количество гостей
-var generateGuests = function () {
-  var guests = Math.floor(Math.random() * 10);
-  if (guests === 0) {
-    guests = 1;
-  }
-  return guests;
-};
+var x = getRandomIntInclusive(0, 1200);
 
-//  генерирует время
-var generateTime = function () {
-  var times = ['12.00', '13.00', '14.00'];
-  var key = Math.floor(Math.random() * 3);
-  return times[key];
-};
+var y = getRandomIntInclusive(130, 630);
 
-//  генерирует массив с удобствами
-var generateFeatures = function () {
-  var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var generatedFeatures = [];
-  var generatedFeaturesLength = Math.floor(Math.random() * features.length);
-  if (generatedFeaturesLength === 0) {
-    generatedFeaturesLength = 1;
-  }
-
-  for (var i = 0; i < features.length; i++) {
-    var key = Math.floor(Math.random() * features.length);
-    generatedFeatures[i] = features[key];
-  }
-  return generatedFeatures;
-};
-
-//  генерирует фотографии
-var generatePhotos = function () {
-  var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  var generatedPhotos = [];
-  var generatedPhotosLength = Math.floor(Math.random() * photos.length);
-  if (generatedPhotosLength === 0) {
-    generatedPhotosLength = 1;
-  }
-  for (var i = 0; i < generatedPhotosLength; i++) {
-    var key = Math.floor(Math.random() * photos.length);
-    generatedPhotos[i] = photos[key];
-  }
-  return generatedPhotos;
-};
-
-//  генерирует координату Х
-var generateX = function () {
-  var screenWidth = document.body.clientWidth;
-  x = Math.floor(Math.random() * screenWidth);
-  return x;
-};
-
-//  генерирует координату У
-var generateY = function () {
-  y = Math.floor(Math.random() * 630);
-  if (y < 130) {
-    y = 130;
-  }
-  return y;
-};
-
-for (var i = 0; i < 8; i++) {
-  offerNumber = i + 1;
-  x = generateX();
-  y = generateY();
-  adverts[i] = {
-    'author': {
-      'avatar': 'img/avatars/user0' + offerNumber + '.png'
+// генерирует объект
+var getItem = function () {
+  var item = {
+    author: {
+      avatar: getRandomUniqueElement (MOCK_DATA.author.avatar)
     },
 
-    'offer': {
-      'title': 'Offer' + offerNumber,
-      'address': x + ',' + y,
-      'price': generatePrice(),
-      'type': generateType(),
-      'rooms': generateRoom(),
-      'guests': generateGuests(),
-      'checkin': generateTime(),
-      'checkout': generateTime(),
-      'features': generateFeatures(),
-      'description': 'Description' + offerNumber,
-      'photos': generatePhotos()
+    offer: {
+      title: getRandomElement (MOCK_DATA.offer.title),
+      address: x + ',' + y,
+      price: getRandomIntInclusive (1000, 10000),
+      type: getRandomElement(MOCK_DATA.offer.type),
+      rooms: getRandomIntInclusive (1, 5),
+      guests: getRandomIntInclusive (1, 10),
+      checkin: getRandomElement (MOCK_DATA.offer.checkin),
+      checkout: getRandomElement (MOCK_DATA.offer.checkout),
+      features: getRandomArrayLength (MOCK_DATA.offer.features),
+      description: getRandomElement (MOCK_DATA.offer.description),
+      photos: getRandomArrayLength(MOCK_DATA.offer.photos)
     },
 
-    'location': {
-      'x': x,
-      'y': y
+    location: {
+      x: x,
+      y: y
     }
   };
+  return item;
 }
 
-//  убираем класс map--faded
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+// генериует массив объектов
+var getAdverts = function () {
+  var array = [];
 
-//  находим блок, в который будем клонировать элементы
-var mapPinsBlock = document.querySelector('.map__pins');
-
-//  находим содержание шаблона
-var mapPinTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
-
-//  создаем фрагмент
-var fragment = document.createDocumentFragment();
-
-
-//  клонируем элементы
-for (i = 0; i < adverts.length; i++) {
-  var mapPin = mapPinTemplate.cloneNode(true);
-  mapPin.querySelector('img').src = adverts[i].author.avatar;
-  mapPin.querySelector('img').alt = adverts[i].offer.title;
-  mapPin.style.left = (adverts[i].location.x - 25) + 'px';
-  mapPin.style.top = (adverts[i].location.y - 70) + 'px';
-
-  fragment.appendChild(mapPin);
+  for (var i = 0; i < MAX_AMOUNT; i++) {
+    array.push(getItem());
+  }
+  return array;
 }
 
-//  вставляем фрагмент в блок
-mapPinsBlock.appendChild(fragment);
+console.log(getAdverts());
+
+// //  убираем класс map--faded
+// var map = document.querySelector('.map');
+// map.classList.remove('map--faded');
+//
+// //  находим блок, в который будем клонировать элементы
+// var mapPinsBlock = document.querySelector('.map__pins');
+//
+// //  находим содержание шаблона
+// var mapPinTemplate = document.querySelector('#pin')
+//     .content
+//     .querySelector('.map__pin');
+//
+// //  создаем фрагмент
+// var fragment = document.createDocumentFragment();
+//
+//
+// //  клонируем элементы
+// for (i = 0; i < adverts.length; i++) {
+//   var mapPin = mapPinTemplate.cloneNode(true);
+//   mapPin.querySelector('img').src = adverts[i].author.avatar;
+//   mapPin.querySelector('img').alt = adverts[i].offer.title;
+//   mapPin.style.left = (adverts[i].location.x - 25) + 'px';
+//   mapPin.style.top = (adverts[i].location.y - 70) + 'px';
+//
+//   fragment.appendChild(mapPin);
+// }
+//
+// //  вставляем фрагмент в блок
+// mapPinsBlock.appendChild(fragment);
